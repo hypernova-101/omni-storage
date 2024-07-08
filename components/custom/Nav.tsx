2aead72a1d7f3f38417415daae758fd18337d60f
hypernova-101 <94484903+hypernova-101@users.noreply.github.com>
2024-07-08 09:15:00 +0530

    Update Nav.tsx

--- components/custom/Nav.tsx
+++ components/custom/Nav.tsx
@@ -5,7 +5,7 @@ function Nav() {
     <nav className="fixed top-0 left-0 min-w-full p-4 z-50 shadow-md flex flex-row gap-4 justify-between items-center dark:bg-zinc-900 border-b-2">
         <div className="flex flex-row gap-4 items-center">
           <Sidebar/>
-          <h1 className="text-2xl font-bold">Messier</h1>
+          <h1 className="text-2xl font-bold">Omni</h1>
         </div>
         
     </nav>
