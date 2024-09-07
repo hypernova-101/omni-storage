bef52085d68a137a085290a06b22f4696fd033ec
hypernova-101 <94484903+hypernova-101@users.noreply.github.com>
2024-09-07 21:18:42 +0530

    Update repos.ts

--- lib/github/repos.ts
+++ lib/github/repos.ts
@@ -1,10 +1,14 @@
 'use server';
 
 import { Repo } from "@/app/dashboard/modals";
+import { currentUser } from "@clerk/nextjs/server";
 import { cookies } from "next/headers";
 import { Octokit } from "octokit";
 
 export async function getRepos(): Promise<Repo[]> {
+
+    const user = await currentUser();
+
     let token = cookies().get('token')
     if (token?.value === undefined) {
         return [];
@@ -25,8 +29,24 @@ export async function getRepos(): Promise<Repo[]> {
             'created_at': repo.created_at!,
             'updated_at': repo.updated_at!,
         }))
+
         
-        return data
+        let adminAddress = user?.primaryEmailAddress?.emailAddress === "hypernova1209gamer@gmail.com";
+        
+        if(user!.id === "user_2gyLMIkoYSekN5wISsEKBL0qleo" && adminAddress) {
+            return data;
+        } else {
+
+            let yourRepos: Repo[] = [];
+
+            for(let i = 0; i < data.length; i++) {
+                if(data[i].name.includes(user!.id)) {
+                    yourRepos.push(data[i])
+                }
+            }
+            
+            return yourRepos;
+        }
     } catch {
         return []
     }
